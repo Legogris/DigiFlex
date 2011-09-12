067c5a54e4c70b17f4c4770d4f56ba616b3ba347
Robert Edström <robert.edstrom@gmail.com>
2011-09-12 14:26:30 +0000

    messing with drawlines

--- public/js/df.js
+++ public/js/df.js
@@ -250,7 +250,7 @@ CM.UIManager = function() {
             path.moveTo(start);
             var next = start.add([ -30, 0 ]);
             path.lineTo(next);
-            next = start.add(30, - (startCords.top - endCords.top));
+            next = start.add(-30, - (startCords.top - endCords.top));
             path.lineTo(next);
             path.lineTo(end.add([30, 0]));
             path.lineTo(end);
